959642408a987adcc76fb2caa0daac31a245a7a8
asaventura <75268261+asaventura@users.noreply.github.com>
2020-12-24 12:12:26 +0300

    Create homework-2-2-dict.py

---
+++ homework-2-2-dict.py
@@ -0,0 +1,3 @@
+month = {1:'Its winter', 2:'Its winter', 3:'Its spring', 4:'Its spring', 5:'Its spring', 6:'Its summer', 7:'Its summer', 8:'Its summer', 9:'Its fall', 10:'Its fall', 11:'Its fall', 12:'Its winter'}
+mon = int(input('Enter number of month: '))
+print(month.get(mon))
