408dfe7985bc564b5aed34b83dde9738259a2d6b
asaventura <75268261+asaventura@users.noreply.github.com>
2021-01-04 19:02:26 +0300

    Update homework-3-4.py

--- homework-3/homework-3-4.py
+++ homework-3/homework-3-4.py
@@ -1,21 +1,18 @@
 def sum_append_func(*args):
-    return sum(args)
+    sum_list1 = [int(arg) for arg in args]
+    return sum(sum_list1)
 
 
-u_input = input("Введите произвольное количество чисел через пробел: ").split(' ')
-sum_list = [int(i) for i in u_input]
-append_list = []
+sum_list = input("Введите произвольное количество чисел через пробел: ").split(' ')
 while True:
     append_input = input(f'Сумма чисел составила {sum_append_func(*sum_list)}. '
                          f'Введите дополнительные числа для сложения '
                          f'и/или введите "exit", чтобы завершить: ').split(' ')
     if 'exit' in append_input:
         append_input.remove('exit')
-        append_list = [int(i) for i in append_input]
-        sum_list.extend(append_list)
+        sum_list.extend(append_input)
         print(f'Сумма всех введенных чисел - {sum_append_func(*sum_list)}, '
               f'программа завершена.')
         break
     else:
-        append_list = [int(i) for i in append_input]
-        sum_list.extend(append_list)
+        sum_list.extend(append_input)
